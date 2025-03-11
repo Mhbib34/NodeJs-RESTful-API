5caeaee01bb66b11a6fe1462b305180c489017fa
Mhbib34 <mhabib34official@gmail.com>
2025-03-11 13:49:39 +0700

    Menambahkan test unit error untuk Create Contact API

--- test/contact.test.js
+++ test/contact.test.js
@@ -33,4 +33,19 @@ describe("POST /api/contacts", function () {
     expect(result.body.data.email).toBe("test@gmail.com");
     expect(result.body.data.phone).toBe("080908090809");
   });
+
+  it("Should reject if request is not invalid", async () => {
+    const result = await supertest(web)
+      .post("/api/contacts")
+      .set("Authorization", "test")
+      .send({
+        firstName: "",
+        lastName: "test",
+        email: "test@gmail.com",
+        phone: "0809080908091231313132131231",
+      });
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
 });
